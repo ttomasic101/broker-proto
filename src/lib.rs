8de00f2751dee8aea0c3fa27d1ed14b916a5c288
Tomislav Tomašić <tomislav.tomasichr@gmail.hr>
2020-05-31 23:53:20 +0200

    Dodani tipovi naredbi

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+#![type_length_limit = "2097152"]
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate rmp_serde as rmps;
+
+#[allow(unused_imports)]
+use bollard::container::*;
 
 
 
@@ -10,7 +14,7 @@ extern crate rmp_serde as rmps;
     #[allow(unused_imports)]
     use rmps::{Deserializer, Serializer};
 
-    #[derive(Debug, PartialEq, Deserialize, Serialize)]
+    #[derive(Debug, Default, Deserialize, Serialize)]
     pub struct Protocol {
         version: Version,
         client: Client,
@@ -19,21 +23,36 @@ extern crate rmp_serde as rmps;
         packet_type: Type,
     }
 
-    #[derive(Debug, PartialEq, Deserialize, Serialize)]
+    #[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
     pub struct Version {
         major: u64,
         minor: u64,
         patch: u64,
     }
 
-    #[derive(Debug, PartialEq, Deserialize, Serialize)]
+    #[derive(Debug, Deserialize, Serialize)]
+    enum Type { Command(Command), Response, Transfer}
+
+    impl Default for Type {
+        fn default() -> Self {
+            Type::Command(Command { cmd_type: CommandType::List})
+        }
+    }
+
+    #[derive(Debug, Default, Deserialize, Serialize)]
     struct Command {
         cmd_type: CommandType,
         
     }
 
     #[derive(Debug, PartialEq, Deserialize, Serialize)]
-    enum CommandType { List, Info, Update, Modify, Stop, Delete, New }
+    pub enum CommandType { List, Change, Container, Log, Stats, Top, }
+
+    impl Default for CommandType {
+        fn default() -> Self {
+            CommandType::List
+        }
+    }
 
     impl Version {
         fn new(major: u64, minor: u64, patch: u64) -> Self {
@@ -44,11 +63,67 @@ extern crate rmp_serde as rmps;
     #[derive(Debug, PartialEq, Deserialize, Serialize)]
     enum Client { Docker, Lxc }
 
-    #[derive(Debug, PartialEq, Deserialize, Serialize)]
-    enum Type { Command, Response, Transfer}
+    impl Default for Client {
+        fn default() -> Self {
+            Client::Docker
+        }
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct ContainerList(bollard::container::APIContainers);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct Change(bollard::container::Change);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct Container(bollard::container::Container);
+
+    //#[derive(Debug, Deserialize, Serialize)]
+    //struct LogOutput(bollard::container::LogOutput);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct Stats(bollard::container::Stats);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct TopResult(bollard::container::TopResult);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    #[serde(remote = "bollard::container::LogOutput")]
+    enum LogOutputWrapper {
+        StdErr {
+            message: String,
+        },
+        StdOut {
+            message: String,
+        },
+        StdIn {
+            message: String,
+        },
+        Console {
+            message: String,
+        },
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    enum Body {
+        ContainerList(ContainerList),
+        Change(Change),
+        Container(Container),
+        #[serde(with = "LogOutputWrapper")]
+        LogOutput(bollard::container::LogOutput),
+        Stats(Stats),
+        TopResult(TopResult),
+        Empty,
+    }
+
+    impl Default for Body {
+        fn default() -> Self {
+            Body::Empty
+        }
+    }
 
     #[derive(Debug, PartialEq, Deserialize, Serialize)]
-    struct Body {}
+    struct Empty {}
 
     impl Protocol {
         pub fn new() -> Self {
@@ -57,7 +132,7 @@ extern crate rmp_serde as rmps;
                 client: Client::Docker,
                 client_version: Some(Protocol::cur_version()),
                 body: Body::new(),
-                packet_type: Type::Command,
+                packet_type: Default::default(),
             }
         }
 
@@ -71,6 +146,13 @@ extern crate rmp_serde as rmps;
             self.serialize(&mut Serializer::new(&mut buf))?;
 
             Ok(buf)
+        }
+
+        pub fn command(cmd: CommandType) -> Protocol {
+            Protocol {
+                packet_type: Type::Command(Command { cmd_type: cmd}),
+                ..Default::default()
+            }
         }
     }
 
@@ -91,7 +173,7 @@ extern crate rmp_serde as rmps;
 
     impl Body {
         fn new() -> Self {
-            Body {}
+            Body::Empty
         }
     }
 
