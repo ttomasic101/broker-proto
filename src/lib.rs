e894ee7094e863da9e83400ac65109d1030709db
Tomislav Tomašić <tomislav.tomasichr@gmail.hr>
2020-06-01 18:41:41 +0200

    Dodane osnovne operacije.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ extern crate serde;
 extern crate serde_derive;
 extern crate rmp_serde as rmps;
 
+extern crate anyhow;
+use anyhow::Result;
+
 #[allow(unused_imports)]
 use bollard::container::*;
 
@@ -14,39 +17,47 @@ use bollard::container::*;
     #[allow(unused_imports)]
     use rmps::{Deserializer, Serializer};
 
-    #[derive(Debug, Default, Deserialize, Serialize)]
+    use bollard::Docker;
+
+    #[derive(Debug, Deserialize, Serialize)]
     pub struct Protocol {
-        version: Version,
-        client: Client,
-        client_version: Option<Version>,
-        body: Body,
-        packet_type: Type,
+        pub version: Version,
+        pub client: Client,
+        pub client_version: Option<ClientVersion>,
+        pub body: Body,
+        pub packet_type: Type,
     }
 
-    #[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+    #[derive(Debug, PartialEq, Deserialize, Serialize)]
     pub struct Version {
         major: u64,
         minor: u64,
         patch: u64,
     }
 
+    type ClientVersion = bollard::system::Version;
+
     #[derive(Debug, Deserialize, Serialize)]
-    enum Type { Command(Command), Response, Transfer}
+    pub enum Type { Command(Command), Response, Transfer, Other}
 
     impl Default for Type {
         fn default() -> Self {
-            Type::Command(Command { cmd_type: CommandType::List})
+            Type::Command(Command { 
+                cmd_type: CommandType::List,
+                name: None
+            })
         }
     }
 
-    #[derive(Debug, Default, Deserialize, Serialize)]
-    struct Command {
-        cmd_type: CommandType,
+    #[derive(Debug, Default, Deserialize, Serialize, Clone)]
+    pub struct Command {
+        pub cmd_type: CommandType,
+        pub name: Option<String>,
         
     }
 
-    #[derive(Debug, PartialEq, Deserialize, Serialize)]
-    pub enum CommandType { List, Change, Container, Log, Stats, Top, }
+    #[derive(Debug, PartialEq, Deserialize, Serialize, Copy, Clone)]
+    pub enum CommandType { List, Change, Container, Log, Stats, Top, Create, Kill, Start, Stop, Prune, Remove, Restart, Update, Wait}
 
     impl Default for CommandType {
         fn default() -> Self {
@@ -61,7 +72,7 @@ use bollard::container::*;
     }
 
     #[derive(Debug, PartialEq, Deserialize, Serialize)]
-    enum Client { Docker, Lxc }
+    pub enum Client { Docker, Lxc }
 
     impl Default for Client {
         fn default() -> Self {
@@ -70,26 +81,31 @@ use bollard::container::*;
     }
 
     #[derive(Debug, Deserialize, Serialize)]
-    struct ContainerList(bollard::container::APIContainers);
+    pub struct ContainerList(pub bollard::container::APIContainers);
 
     #[derive(Debug, Deserialize, Serialize)]
-    struct Change(bollard::container::Change);
+    pub struct Change(pub bollard::container::Change);
 
     #[derive(Debug, Deserialize, Serialize)]
-    struct Container(bollard::container::Container);
+    pub struct Container(pub bollard::container::Container);
 
     //#[derive(Debug, Deserialize, Serialize)]
     //struct LogOutput(bollard::container::LogOutput);
 
     #[derive(Debug, Deserialize, Serialize)]
-    struct Stats(bollard::container::Stats);
+    pub struct Stats(pub bollard::container::Stats);
 
     #[derive(Debug, Deserialize, Serialize)]
-    struct TopResult(bollard::container::TopResult);
+    pub struct TopResult(pub bollard::container::TopResult);
 
     #[derive(Debug, Deserialize, Serialize)]
-    #[serde(remote = "bollard::container::LogOutput")]
-    enum LogOutputWrapper {
+    pub struct PruneContainerResults(pub bollard::container::PruneContainersResults);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct CreateContainerResults(pub bollard::container::CreateContainerResults);
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub enum LogOutputWrapper {
         StdErr {
             message: String,
         },
@@ -105,14 +121,16 @@ use bollard::container::*;
     }
 
     #[derive(Debug, Deserialize, Serialize)]
-    enum Body {
-        ContainerList(ContainerList),
-        Change(Change),
+    pub enum Body {
+        ContainerList(Vec<ContainerList>),
+        Change(Option<Vec<Change>>),
         Container(Container),
-        #[serde(with = "LogOutputWrapper")]
-        LogOutput(bollard::container::LogOutput),
-        Stats(Stats),
+        LogOutput(Vec<LogOutputWrapper>),
+        Stats(Vec<Stats>),
         TopResult(TopResult),
+        Error(String),
+        PrunedContainers(PruneContainerResults),
+        CreateContainerResults(CreateContainerResults),
         Empty,
     }
 
@@ -126,6 +144,7 @@ use bollard::container::*;
     struct Empty {}
 
     impl Protocol {
+        /*
         pub fn new() -> Self {
             Protocol {
                 version: Version::new(1, 0, 0),
@@ -135,6 +154,7 @@ use bollard::container::*;
                 packet_type: Default::default(),
             }
         }
+        */
 
         pub fn cur_version() -> Version {
             Version::new(1, 0, 0)
@@ -148,12 +168,239 @@ use bollard::container::*;
             Ok(buf)
         }
 
-        pub fn command(cmd: CommandType) -> Protocol {
+        pub async fn command(cmd: CommandType) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: cmd,
+                    name: None }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn list() -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::List,
+                    name: None }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn prune() -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Prune,
+                    name: None }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn create() -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Create,
+                    name: None }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn change(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Change,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn inspect(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Container,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn logs(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Log,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn stats(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Stats,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn stop(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Stop,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn start(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Start,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn kill(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Kill,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn restart(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Restart,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn top(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Top,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn remove(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Remove,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn update(name: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Command(Command { 
+                    cmd_type: CommandType::Update,
+                    name: Some(String::from(name)) }),
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn response(docker: &Docker) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Response,
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: Some(docker.version().await?),
+                body: Body::Empty
+            })
+        }
+
+        pub async fn error(docker: &Docker, err: &str) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Response,
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: Some(docker.version().await?),
+                body: Body::Error(String::from(err))
+            })
+        }
+
+        pub fn error_none(err: &str) -> Protocol {
             Protocol {
-                packet_type: Type::Command(Command { cmd_type: cmd}),
-                ..Default::default()
+                packet_type: Type::Response,
+                version: Protocol::cur_version(),
+                client: Client::Docker,
+                client_version: None,
+                body: Body::Error(String::from(err))
             }
         }
+
+        pub async fn default(docker: &Docker) -> Result<Protocol> {
+            Ok(Protocol {
+                packet_type: Type::Other,
+                version: Protocol::cur_version(),
+                client_version: Some(docker.version().await?),
+                client: Client::Docker,
+                body: Body::Empty
+            })
+        }
+
+        pub async fn default_none() -> Result<Protocol> {
+            Ok( Protocol {
+                packet_type: Type::Other,
+                version: Protocol::cur_version(),
+                client_version: None,
+                client: Client::Docker,
+                body: Body::Empty
+            })
+        }
+
+        
+
+        
     }
 
     use std::convert::TryFrom;
@@ -168,12 +415,6 @@ use bollard::container::*;
             let mut de = Deserializer::new(cursor);
 
             Deserialize::deserialize(&mut de)
-        }
-    }
-
-    impl Body {
-        fn new() -> Self {
-            Body::Empty
         }
     }
 
